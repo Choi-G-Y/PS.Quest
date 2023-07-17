19e1d438331b1a0549c1f73748c2f29270290a5e
CGY <lemong723@gmail.com>
2023-07-17 18:00:19 +0900

    0717
    JS Study

--- H/JS/JStudy3/myScript3.js
+++ H/JS/JStudy3/myScript3.js
@@ -5,3 +5,13 @@ function myFunction(p1, p2, p3) {
 
 let result = myFunction(4, 3);
 document.getElementById("test").innerHTML = result;
+
+function myChange() {
+    let change = document.getElementById("string_Chg").innerHTML;
+    document.getElementById("string_Chg").innerHTML = change.replace("노력", "재능");
+    // document.getElementById("string_Chg").innerHTML = change.replace("재능", "노력");
+}
+
+function quest() {
+    document.getElementById("hello").innerHTML = "hello world";
+}
